3bc407aa02505d87694ba7e33e81fbfd0ae175a6
Vincent Aldiano <vincentaldiano@gmail.com>
2021-12-13 12:56:19 -0600

    commit tree a72b76fabfcc5356f709776be320883c99570be3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+// code your solution here}!`;
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun('bathe my dog');
+
+function mondayWork(toDo = 'go to the office') {
+    return `This Monday, I will ${toDo}.`;
+}
+mondayWork('work from home');
+
+function wrapAdjective(string = '*') {
+    return function(parameter = 'special') {
+        return `You are ${string}${parameter}${string}!`
+    }
+}
+wrapAdjective("*")("a hard worker")("*");
+wrapAdjective('||')('a dedicated programmer')('||');
